9c6deb706f2a3a4dd81667bfc0eba652728d1af9
Jonhnanthan Oliveira <jonhnanthan@gmail.com>
2012-04-25 14:22:14 -0300

    testes atualizados

--- src/testes/TesteEasy.java
+++ src/testes/TesteEasy.java
@@ -10,9 +10,9 @@ public class TesteEasy {
 	public static void main(String[] args) throws Exception {
 
 		List<String> files = new ArrayList<String>();
-//		files.add("scripts/US01.txt");
+//		files.add("scripts/US01.txt"); OK
 //		files.add("scripts/US02.txt");
-		files.add("scripts/US03.txt");
+//		files.add("scripts/US03.txt"); OK
 //		files.add("scripts/US04.txt");
 //		files.add("scripts/US05.txt");
 
